fc96e9553b0da87d16746a3a42bbb3b819ec568f
MrBackend <roy.brokvam@gmail.com>
2017-05-15 23:11:40 +0200

    Less code by eliminating the Step class

--- src/main/java/org/jstuff/trampoline/Trampoline.java
+++ src/main/java/org/jstuff/trampoline/Trampoline.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
- * A class that represents a (possibly recursive) calculation which, when run, will not overflow the stack.
+ * A class that represents a calculation which, when run, will not overflow the stack.
  *
  * @param <A> The type of the calculated value
  */
@@ -15,7 +15,7 @@ public abstract class Trampoline<A> {
      * Creates a Trampoline that represents a constant value.
      *
      * @param value The value which will be returned by this Trampoline.
-     * @param <A> The type of the value
+     * @param <A>   The type of the value
      * @return A new Trampoline that represents the value.
      */
     public static <A> Trampoline<A> ret(A value) {
@@ -26,7 +26,7 @@ public abstract class Trampoline<A> {
      * Creates a Trampoline that represents a thunk (an unevaluated calculation).
      *
      * @param thunk A Supplier that will return a value
-     * @param <A> The type of the value
+     * @param <A>   The type of the value
      * @return A new Trampoline that represents the unevaluated calculation.
      */
     public static <A> Trampoline<A> suspend(Supplier<Trampoline<A>> thunk) {
@@ -43,31 +43,31 @@ public abstract class Trampoline<A> {
      * @return The calculated value
      */
     public final A run() {
-        Step<A> step = resume();
-        while (step.isIncomplete()) {
-            step = step.getNext();
+        Trampoline<A> curr = this;
+        while (!(curr instanceof Return)) {
+            curr = curr.resume();
         }
-        return step.getValue();
+        return ((Return<A>) curr).value;
     }
 
     /**
-     * Creates a Trampoline that represents a future evaluation of this Trampoline, followed by a calculation
+     * Creates a Trampoline that represents an evaluation of this Trampoline, followed by a calculation
      * that transforms the result of this Trampoline to another value.
      *
-     * @param f The function used to transform the result of this Trampoline
+     * @param f   The function used to transform the result of this Trampoline
      * @param <B> The type of the Transformed value
      * @return A new Trampoline that represents the application of f to this Trampoline
      */
     public final <B> Trampoline<B> map(Function<A, B> f) {
-        return flatMap(f.andThen(Trampoline::ret));
+        return flatMap(a -> ret(f.apply(a)));
     }
 
     /**
-     * Creates a Trampoline that represents a future evaluation of this Trampoline, followed by a calculation
+     * Creates a Trampoline that represents an evaluation of this Trampoline, followed by a calculation
      * that produces a new Trampoline. This can be viewed as the sequence of two Trampolines, where the second
      * Trampoline depends on the result of this Trampoline.
      *
-     * @param f The function used to calculate the next Trampoline
+     * @param f   The function used to calculate the next Trampoline
      * @param <B> The type of the value returned by the next Trampoline (and, consequently, by the new Trampoline)
      * @return A new Trampoline that represents the sequence of this Trampoline followed by the next Trampoline
      */
@@ -75,9 +75,16 @@ public abstract class Trampoline<A> {
         return new FlatMap<>(this, f);
     }
 
-    abstract Step<A> resume();
+    /*
+     * resume(Return(x)) = Return(x)
+     * resume(Suspend(() -> a)) = a
+     * resume(FlatMap(Return(x), f)) = f.apply(x)
+     * resume(FlatMap(Suspend(() -> a), f)) = FlatMap(a, f)
+     * resume(FlatMap(FlatMap(a, x -> b), f)) = FlatMap(a, x -> FlatMap(b, f))
+     */
+    abstract Trampoline<A> resume();
 
-    abstract <B> Trampoline<B> makeTailRec(Function<A, Trampoline<B>> f);
+    abstract <B> Trampoline<B> applyFlatMap(Function<A, Trampoline<B>> f);
 
 
     private static final class Return<A> extends Trampoline<A> {
@@ -89,12 +96,12 @@ public abstract class Trampoline<A> {
         }
 
         @Override
-        Step<A> resume() {
-            return new Done<>(value);
+        Trampoline<A> resume() {
+            return this;
         }
 
         @Override
-        <B> Trampoline<B> makeTailRec(Function<A, Trampoline<B>> f) {
+        <B> Trampoline<B> applyFlatMap(Function<A, Trampoline<B>> f) {
             return f.apply(value);
         }
 
@@ -110,12 +117,12 @@ public abstract class Trampoline<A> {
         }
 
         @Override
-        Step<A> resume() {
-            return new Incomplete<>(thunk.get());
+        Trampoline<A> resume() {
+            return thunk.get();
         }
 
         @Override
-        <B> Trampoline<B> makeTailRec(Function<A, Trampoline<B>> f) {
+        <B> Trampoline<B> applyFlatMap(Function<A, Trampoline<B>> f) {
             return thunk.get().flatMap(f);
         }
 
@@ -128,81 +135,18 @@ public abstract class Trampoline<A> {
         private final Function<X, Trampoline<A>> nextTrampolineF;
 
         private FlatMap(Trampoline<X> trampoline, Function<X, Trampoline<A>> nextTrampolineF) {
-            this.trampoline = Objects.requireNonNull(trampoline);
-            this.nextTrampolineF = Objects.requireNonNull(nextTrampolineF);
-        }
-
-        @Override
-        Step<A> resume() {
-            return new Incomplete<>(trampoline.makeTailRec(nextTrampolineF));
-        }
-
-        @Override
-        <B> Trampoline<B> makeTailRec(Function<A, Trampoline<B>> f) {
-            return trampoline.flatMap(x -> nextTrampolineF.apply(x).flatMap(f));
-        }
-
-    }
-
-
-    private abstract static class Step<A> {
-
-        abstract boolean isIncomplete();
-
-        abstract Step<A> getNext();
-
-        abstract A getValue();
-
-    }
-
-
-    private static final class Incomplete<A> extends Step<A> {
-
-        private final Trampoline<A> trampoline;
-
-        private Incomplete(Trampoline<A> trampoline) {
             this.trampoline = trampoline;
+            this.nextTrampolineF = nextTrampolineF;
         }
 
         @Override
-        boolean isIncomplete() {
-            return true;
+        Trampoline<A> resume() {
+            return trampoline.applyFlatMap(nextTrampolineF);
         }
 
         @Override
-        Step<A> getNext() {
-            return trampoline.resume();
-        }
-
-        @Override
-        A getValue() {
-            throw new IllegalStateException();
-        }
-
-    }
-
-
-    private static final class Done<A> extends Step<A> {
-
-        private final A value;
-
-        private Done(A value) {
-            this.value = value;
-        }
-
-        @Override
-        boolean isIncomplete() {
-            return false;
-        }
-
-        @Override
-        Step<A> getNext() {
-            throw new IllegalStateException();
-        }
-
-        @Override
-        A getValue() {
-            return value;
+        <B> Trampoline<B> applyFlatMap(Function<A, Trampoline<B>> f) {
+            return trampoline.flatMap(x -> nextTrampolineF.apply(x).flatMap(f));
         }
 
     }
